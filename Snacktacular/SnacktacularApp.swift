aa7fd745f149176d356624f58316d53ae5dd8da3
Steven Yung <steve@redricebowl.com>
2023-09-26 14:36:41 -0700

    Initial Commit

---
+++ Snacktacular/SnacktacularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularApp.swift
+//  Snacktacular
+//
+//  Created by Steven Yung on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
