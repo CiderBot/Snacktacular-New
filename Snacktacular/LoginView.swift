a39f9e7d6987560f70b7fd5be10d3cb17200d189
Steven Yung <steve@redricebowl.com>
2023-09-27 12:05:46 -0700

    Login screen: navigation between fields, simple email and password check

--- Snacktacular/LoginView.swift
+++ Snacktacular/LoginView.swift
@@ -9,10 +9,16 @@ import SwiftUI
 import Firebase
 
 struct LoginView: View {
+    enum Field {    // use to tab between textfields
+        case email, password
+    }
+    @FocusState private var focusField: Field?
+    
     @State private var email = ""
     @State private var password = ""
     @State private var showAlert = false
     @State private var alertMessage = ""
+    @State private var buttonDisabled = true
     
     var body: some View {
         NavigationStack {
@@ -27,10 +33,24 @@ struct LoginView: View {
                     .autocorrectionDisabled()
                     .textInputAutocapitalization(.never)
                     .submitLabel(.next)
+                    .focused($focusField, equals: .email)
+                    .onSubmit {
+                        focusField = .password
+                    }
+                    .onChange(of: email) {
+                        enableButtons()
+                    }
                 
                 SecureField("Password", text: $password)
                     .textInputAutocapitalization(.never) // probably not needed
                     .submitLabel(.done)
+                    .focused($focusField, equals: .password)
+                    .onSubmit {
+                        focusField = nil
+                    }
+                    .onChange(of: password) {
+                        enableButtons()
+                    }
             }
             .textFieldStyle(.roundedBorder)
             .overlay {
@@ -54,6 +74,7 @@ struct LoginView: View {
                 }
                 .padding(.leading)
             }
+            .disabled(buttonDisabled)
             .buttonStyle(.borderedProminent)
             .tint(Color("SnackColor"))
             .font(.title2)
@@ -89,6 +110,13 @@ struct LoginView: View {
                 //TODO: load list view
             }
         }
+    }
+    
+    func enableButtons() {
+        // very simple email and password validation, assuming min 6 characters in both email and password
+        let emailIsGood = email.count >= 6 && email.contains("@")
+        let passwordIsGood = password.count >= 6
+        buttonDisabled = !(emailIsGood && passwordIsGood)
     }
 }
 
