534dd6d3ad004bd3aa4a627a7ec98654cc02e9e2
Steven Yung <steve@redricebowl.com>
2023-09-29 10:19:26 -0700

    Continue location stuff

--- Snacktacular/Model/PlaceViewModel.swift
+++ Snacktacular/Model/PlaceViewModel.swift
@@ -1,30 +1,8 @@
 //
 //  PlaceViewModel.swift
-//  PlaceLookupDemo
+//  Snacktacular
 //
-//  Created by Steven Yung on 9/28/23.
+//  Created by Steven Yung on 9/29/23.
 //
 
 import Foundation
-import MapKit
-
-@MainActor
-class PlaceViewModel: ObservableObject {
-    @Published var places: [Place] = []
-    
-    func search(text: String, region: MKCoordinateRegion) {
-        let searchRequest = MKLocalSearch.Request()
-        searchRequest.naturalLanguageQuery = text
-        searchRequest.region = region
-        let search = MKLocalSearch(request: searchRequest)
-        
-        search.start { response, error in
-            guard let response = response else {
-                print("😡 ERROR: \(error?.localizedDescription ?? "Unknown Location Search Error")")
-                      return
-            }
-            
-            self.places = response.mapItems.map(Place.init)  // puts each search item found into the places array
-        }
-    }
-}
---
+++ Snacktacular/PlaceLookupView.swift
@@ -0,0 +1,18 @@
+//
+//  PlaceLookupView.swift
+//  Snacktacular
+//
+//  Created by Steven Yung on 9/29/23.
+//
+
+import SwiftUI
+
+struct PlaceLookupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlaceLookupView()
+}
